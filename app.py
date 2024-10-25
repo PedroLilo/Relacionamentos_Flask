73426a90be43ed99a0f9597bc333ff35ab895afa
PedroLilo <pedrohenriquelilo557@gmail.com>
2024-10-25 10:29:30 -0300

    Modularizando

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from database import db
+from flask_migrate import Migrate
+from models import Usuario, Pedido, Pizza
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "QualquerCoisaSecreta"
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/bim3g1"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+migrate = Migrate(app, db)
